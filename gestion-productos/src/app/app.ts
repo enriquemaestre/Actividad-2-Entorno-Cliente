85b7452c85f78780d4f7b2121b64dbe62e3568a8
Enrique Maestre <enrique.maestre265@comunidadunir.net>
2025-12-15 23:05:59 +0100

    Mod: incluimos bootstrap en angular.json para que sea interpretado y terminamos la configuración y código faltantes para products-list y app .ts y .html

--- gestion-productos/src/app/app.ts
+++ gestion-productos/src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProductService, Product } from './services/product';
+import { ProductsList } from './components/products-list/products-list';
+import { ProductFormComponent } from './components/product-form/product-form';
+import { ProductFilterComponent } from './components/product-filter/product-filter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProductsList,ProductFormComponent,ProductFilterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('gestion-productos');
+
+  productos: Product[] = [];
+
+  constructor(private productService: ProductService) {
+  }
+
+  onProductoCreado(producto: any) {
+    this.productService.agregarProducto(producto);
+  }
 }

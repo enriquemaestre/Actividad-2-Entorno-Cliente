8955e2fac9859b88963e545bb7a4542560865b97
Enrique Maestre <enrique.maestre265@comunidadunir.net>
2025-12-15 20:58:18 +0100

    Add: creación de los componentes necesarios

---
+++ gestion-productos/src/app/components/product-filter/product-filter.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-filter',
+  imports: [],
+  templateUrl: './product-filter.html',
+  styleUrl: './product-filter.css',
+})
+export class ProductFilter {
+
+}
